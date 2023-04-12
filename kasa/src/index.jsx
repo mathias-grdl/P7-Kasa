299f7e4b90cac45a8b7140b56854177775d2a396
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-04-12 16:40:20 +0200

    react-router BUG

---
+++ kasa/src/index.jsx
@@ -0,0 +1,24 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import Home from './pages/Home'
+// import About from './pages/About'
+// import Header from './components/Header'
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Header />
+//       <Switch>
+//         <Route exact path="/">
+//           <Home />
+//         </Route>
+//         <Route path="/About">
+//           <About />
+//         </Route>
+//       </Switch>
+//     </Router>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
---
+++ kasa/src/pages/About/index.jsx
@@ -0,0 +1,5 @@
+function About() {
+    return <div>Apropos</div>
+  }
+  
+  export default About
