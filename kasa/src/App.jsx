68bebcff528ce5f49a4f364b0390146ff9640cec
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-05-14 10:36:58 +0200

    Replacement du footer

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -4,20 +4,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Header from './components/Header'
-import Footer from './components/Footer'
+import Footer from './components/Footer/index'
 import ErrorPage from './pages/Error/index'
 
 function App() {
   return (
-    <main>
-      <Header />
-      <Routes> 
-        <Route path="/" element={<Home />} />
-        <Route path="/apropos" element={<About />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+    <div>
+      <main>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/apropos" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </main>
       <Footer />
-    </main> 
+    </div>
   )
 }
 
