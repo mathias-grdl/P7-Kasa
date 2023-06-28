b76f8d091ce262cfabd4b792c7cb2ae2949de2ef
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-06-28 10:14:43 +0200

    correction de housting par housing

--- kasa/src/pages/Housing/index.jsx
+++ kasa/src/pages/Housing/index.jsx
@@ -3,25 +3,45 @@ import Profil from "../../components/Profil";
 import Slider from "../../components/Slider";
 import Tags from "../../components/Tags";
 import { useParams } from "react-router-dom";
+// import { useState, useEffect } from "react";
 import data from "../../data/data.json";
 import "./index.css";
 
-function Housting() {
+function Housing() {
+
+  // // Stocker les données de l'API. La valeur initiale de useState est un tableau vide
+  // const [data, setData] = useState([]);
+
+  // // Fonction asynchrone pour récupérer les données
+  // const getData = async () => {
+  //   // Effectue une requête pour récupérer le fichier JSON
+  //   const response = await fetch(
+  //     "data.json"
+  //   ).then((response) => response.json()); //Extrait les données JSON de la réponse
+    
+  //   setData(response); // Met à jour l'état avec les données récupérées
+  // };
+
+  // // Execute getData() au chargement de la page.
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+
   const { id } = useParams();
-  const housting = data.find((housting) => housting.id === id);
+  const housing = data.find((housing) => housing.id === id);
 
   return (
-    <div className="housting">
-      <Slider pictures={housting.cover} />
+    <div className="housing">
+      <Slider pictures={housing.cover} />
       <div className="houstingInfo">
         <div className="houstingTitle">
-          <h1>{housting.title}</h1>
-          <p>{housting.location}</p>
-          <Tags tags={housting.tags} />
+          <h1>{housing.title}</h1>
+          <p>{housing.location}</p>
+          <Tags tags={housing.tags} />
         </div>
 
         <div>
-          <Profil name={housting.host.name} picture={housting.host.picture} />
+          <Profil name={housing.host.name} picture={housing.host.picture} />
           {/* <Stars/> */}
         </div>
       </div>
@@ -30,4 +50,4 @@ function Housting() {
   );
 }
 
-export default Housting;
+export default Housing;
