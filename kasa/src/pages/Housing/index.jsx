9d857f67bc9859e2b60ecccb44a45414e18bc8cc
mathias-grdl <114570081+mathias-grdl@users.noreply.github.com>
2023-07-09 18:52:46 +0200

    Remplacement de l'import par un fetch

--- kasa/src/pages/Housing/index.jsx
+++ kasa/src/pages/Housing/index.jsx
@@ -4,31 +4,46 @@ import Rating from "../../components/Rating";
 import Slider from "../../components/Slider";
 import Tags from "../../components/Tags";
 import { useParams } from "react-router-dom";
-// import { useState, useEffect } from "react";
-import data from "../../data/data.json";
+import { useState, useEffect } from "react";
 import "./index.css";
 
 function Housing() {
-  // // Stocker les données de l'API. La valeur initiale de useState est un tableau vide
-  // const [data, setData] = useState([]);
+  // Stocker les données de l'API. La valeur initiale de useState est un tableau vide
+  const [data, setData] = useState(null);
 
-  // // Fonction asynchrone pour récupérer les données
-  // const getData = async () => {
-  //   // Effectue une requête pour récupérer le fichier JSON
-  //   const response = await fetch(
-  //     "data.json"
-  //   ).then((response) => response.json()); //Extrait les données JSON de la réponse
+  // Fonction asynchrone pour récupérer les données
+  const getData = async () => {
+    try {
+      // Effectue une requête pour récupérer le fichier JSON
+      const response = await fetch("http://localhost:3000/data.json");
 
-  //   setData(response); // Met à jour l'état avec les données récupérées
-  // };
+      if (!response.ok) {
+        //.ok est une propriété booléenne de l'objet Response
+        throw new Error("Erreur lors de la récupération des données.");
+      }
 
-  // // Execute getData() au chargement de la page.
-  // useEffect(() => {
-  //   getData();
-  // }, []);
+      const jsonData = await response.json(); //Extrait les données JSON de la réponse
 
+      setData(jsonData); // Met à jour l'état avec les données récupérées
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Execute getData() au chargement de la page.
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // console.log(data);
   const { id } = useParams();
-  const housing = data.find((housing) => housing.id === id);
+  const housing = data && data.find((housing) => housing.id === id);
+
+  console.log(housing);
+
+  if (!housing) {
+    return <p>Chargement en cours...</p>;
+  }
 
   const dataDescription = [
     {
@@ -60,12 +75,12 @@ function Housing() {
         </div>
       </div>
       <div className="housingAccordionContainer">
-      <div className="housingAccordion">
-      <Accordion data={dataDescription} />
-      </div>
-      <div className="housingAccordion">
-      <Accordion data={dataEquipments} />
-      </div>
+        <div className="housingAccordion">
+          <Accordion data={dataDescription} />
+        </div>
+        <div className="housingAccordion">
+          <Accordion data={dataEquipments} />
+        </div>
       </div>
     </div>
   );
